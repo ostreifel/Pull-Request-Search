f74cb1eb52903d9e0d513e07395e512891ac6704
Otto Streifel <ottost@microsoft.com>
2016-11-02 20:58:37 -0700

    Identity picker

--- scripts/app.ts
+++ scripts/app.ts
@@ -1,3 +1,9 @@
+import {Combo} from "VSS/Controls/Combos";
+import {IdentityPickerSearchControl, IIdentityPickerSearchOptions} from "VSS/Identities/Picker/Controls";
+import {BaseControl} from "VSS/Controls";
+
+const idOptions: IIdentityPickerSearchOptions = {};
+BaseControl.createIn(IdentityPickerSearchControl, $('.id-picker'), idOptions);
 var actionProvider =  {
 };
 
