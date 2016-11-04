26bd71f77443a9313c405887d00de8f57d570855
Otto Streifel <ottost@microsoft.com>
2016-11-04 09:42:40 -0700

    Updates dependencies

--- scripts/app.ts
+++ scripts/app.ts
@@ -1,10 +1,16 @@
-import {Combo} from "VSS/Controls/Combos";
-import {IdentityPickerSearchControl, IIdentityPickerSearchOptions} from "VSS/Identities/Picker/Controls";
-import {BaseControl} from "VSS/Controls";
+import {getClient as getCoreClient} from "TFS/Core/RestClient";
+import {getClient as getGitClient} from "TFS/VersionControl/GitRestClient";
+import * as React from "React";
 
-const idOptions: IIdentityPickerSearchOptions = {};
-BaseControl.createIn(IdentityPickerSearchControl, $('.id-picker'), idOptions);
-var actionProvider =  {
-};
 
-VSS.register(VSS.getExtensionContext().extensionId, actionProvider);
+const gitClient = getGitClient();
+const coreClient = getCoreClient();
+
+const projectId = VSS.getWebContext().project.id;
+const teamId = VSS.getWebContext().team.id;
+// limits to 100
+coreClient.getTeamMembers(projectId, teamId).then((teamMembers) => {
+    console.log(teamMembers);
+});
+
+VSS.register(VSS.getExtensionContext().extensionId, {});
