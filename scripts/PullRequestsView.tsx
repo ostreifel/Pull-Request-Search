33b3f9fd1d8303942299692716a4636a3152e2e1
Otto Streifel <ottost@microsoft.com>
2016-12-15 18:21:36 -0800

    tslint

--- scripts/PullRequestsView.tsx
+++ scripts/PullRequestsView.tsx
@@ -4,19 +4,19 @@ import * as React from "react";
 import * as Utils_Date from "VSS/Utils/Date";
 
 export interface ICallbacks {
-    creator: (displayName: string) => void
-    reviewer: (displayName: string) => void
+    creator: (displayName: string) => void;
+    reviewer: (displayName: string) => void;
 }
 
 function computeApprovalStatus(reviewers: IdentityRefWithVote[]): string {
 
-    if ($.grep(reviewers, (reviewer) => reviewer.vote == -10).length > 0) {
+    if ($.grep(reviewers, (reviewer) => reviewer.vote === -10).length > 0) {
         return "Rejected";
-    } else if ($.grep(reviewers, (reviewer) => reviewer.vote == -5).length > 0) {
+    } else if ($.grep(reviewers, (reviewer) => reviewer.vote === -5).length > 0) {
         return "Awaiting Author";
-    } else if ($.grep(reviewers, (reviewer) => reviewer.vote == 5).length > 0) {
+    } else if ($.grep(reviewers, (reviewer) => reviewer.vote === 5).length > 0) {
         return "Approved with suggestions";
-    } else if ($.grep(reviewers, (reviewer) => reviewer.vote == 10).length > 0) {
+    } else if ($.grep(reviewers, (reviewer) => reviewer.vote === 10).length > 0) {
         return "Approved";
     } else {
         return "Awaiting Approval";
@@ -31,7 +31,7 @@ class RequestRow extends React.Component<{ pullRequest: GitPullRequest }, void> 
         const project = VSS.getWebContext().project.name;
         const team = VSS.getWebContext().team.name;
         const url = `${uri}${project}/${team}/_git/${pr.repository.name}/pullrequest/${pr.pullRequestId}`;
-        const targetName = pr.targetRefName.replace('refs/heads/', '');
+        const targetName = pr.targetRefName.replace("refs/heads/", "");
         const createTime = Utils_Date.friendly(pr.creationDate);
 
         const approvalStatus = computeApprovalStatus(pr.reviewers);
@@ -41,13 +41,13 @@ class RequestRow extends React.Component<{ pullRequest: GitPullRequest }, void> 
         );
         return (
             <tr className="pr-row">
-                <td><img src={pr.createdBy.imageUrl} title={pr.createdBy.displayName}/></td>
+                <td><img src={pr.createdBy.imageUrl} title={pr.createdBy.displayName} /></td>
                 <td>
-                    <a href={url} target={'_blank'}>{pr.title}</a>
+                    <a href={url} target={"_blank"}>{pr.title}</a>
                     <div>{`${pr.createdBy.displayName} requested #${pr.pullRequestId} into ${targetName} ${createTime}`}</div>
                 </td>
                 <td className="skinny-column">
-                    {pr.status == PullRequestStatus.Active ? approvalStatus : PullRequestStatus[pr.status]}
+                    {pr.status === PullRequestStatus.Active ? approvalStatus : PullRequestStatus[pr.status]}
                 </td>
                 <td className="skinny-column">
                     {pr.repository.name}
@@ -79,10 +79,14 @@ class InfoHeader extends React.Component<void, void> {
     render() {
         return (
             <div>
-                <a href={'https://marketplace.visualstudio.com/items?itemName=ottostreifel.pull-request-search'}  target={'_blank'}>{'Pull Request Search'}</a>{' extension '} 
-                <a href={'https://github.com/ostreifel/Pull-Request-Search'} target={'_blank'}>{'v' + VSS.getExtensionContext().version}</a>{' '}
-                <a href={'https://github.com/ostreifel/Pull-Request-Search/issues'} target={'_blank'}>Report an issue</a>{' '}
-                <a href={'mailto:prsearchextension@microsoft.com'} target={'_blank'}>Feedback and questions</a>
+                <a href={"https://marketplace.visualstudio.com/items?itemName=ottostreifel.pull-request-search"} target={"_blank"}>
+                    {"Pull Request Search"}
+                </a> {" extension "}
+                <a href={"https://github.com/ostreifel/Pull-Request-Search"} target={"_blank"}>
+                    {"v" + VSS.getExtensionContext().version}
+                </a>{" "}
+                <a href={"https://github.com/ostreifel/Pull-Request-Search/issues"} target={"_blank"}>Report an issue</a>{" "}
+                <a href={"mailto:prsearchextension@microsoft.com"} target={"_blank"}>Feedback and questions</a>
             </div>
         );
     }
@@ -90,7 +94,7 @@ class InfoHeader extends React.Component<void, void> {
 
 export function renderResults(pullRequests: GitPullRequest[], filter: (pr: GitPullRequest) => boolean, getMore: () => void) {
     let bodyElem: JSX.Element;
-    if (pullRequests.length == 0) {
+    if (pullRequests.length === 0) {
         bodyElem = <div>No pull requests found</div>;
     } else {
         const filtered = pullRequests.filter(filter);
@@ -98,7 +102,7 @@ export function renderResults(pullRequests: GitPullRequest[], filter: (pr: GitPu
             <RequestsView pullRequests={filtered} />
             <div>{`${filtered.length}/${pullRequests.length} pull requests match title and date criteria. `}
                 <a onClick={getMore}>
-                    {pullRequests.length % 100 === 0 ? 'Search more items' : ''}
+                    {pullRequests.length % 100 === 0 ? "Search more items" : ""}
                 </a>
             </div>
         </div>;
@@ -112,7 +116,7 @@ export function renderResults(pullRequests: GitPullRequest[], filter: (pr: GitPu
     );
 
     ReactDom.render(
-        <InfoHeader/>,
+        <InfoHeader />,
         document.getElementById("header")
-    )
+    );
 }
--- scripts/app.ts
+++ scripts/app.ts
@@ -1,59 +1,34 @@
-import {Combo, IComboOptions} from "VSS/Controls/Combos";
-import {IdentityPickerSearchControl, IIdentityPickerSearchOptions} from "VSS/Identities/Picker/Controls";
-import {BaseControl} from "VSS/Controls";
-import {getClient as getCoreClient} from "TFS/Core/RestClient";
-import {getClient as getGitClient} from "TFS/VersionControl/GitRestClient";
-import {GitPullRequestSearchCriteria, PullRequestStatus, GitPullRequest, GitRepository} from "TFS/VersionControl/Contracts";
-import {renderResults} from "./PullRequestsView";
-import {EntityFactory} from "VSS/Identities/Picker/Controls";
-import {CommonIdentityPickerHttpClient, IEntity} from "VSS/Identities/Picker/RestClient";
-import {getService} from "VSS/Service";
-import {IdentityService, IOperationScope, IEntityType} from "VSS/Identities/Picker/Services";
-import * as Q from "q";
+import { Combo, IComboOptions } from "VSS/Controls/Combos";
+import { IdentityPickerSearchControl, IIdentityPickerSearchOptions } from "VSS/Identities/Picker/Controls";
+import { BaseControl } from "VSS/Controls";
+import { getClient as getGitClient } from "TFS/VersionControl/GitRestClient";
+import { GitPullRequestSearchCriteria, PullRequestStatus, GitPullRequest, GitRepository } from "TFS/VersionControl/Contracts";
+import { renderResults } from "./PullRequestsView";
+import { IEntity } from "VSS/Identities/Picker/RestClient";
 
-//Create controls
+// create controls
 const statusOptions: IComboOptions = {
-    source: ['Active', 'Abandoned', 'Completed', 'All'],
-    value: 'Active',
+    source: ["Active", "Abandoned", "Completed", "All"],
+    value: "Active",
 };
-const statusControl = <Combo>BaseControl.createIn(Combo, $('.status-picker'), statusOptions);
+const statusControl = <Combo>BaseControl.createIn(Combo, $(".status-picker"), statusOptions);
 
 const idOptions: IIdentityPickerSearchOptions = {
-    identityType: {User: true, Group: true},
-}
-const creatorControl = <IdentityPickerSearchControl>BaseControl.createIn(IdentityPickerSearchControl, $('.creator-picker'), idOptions);
-const reviewerControl = <IdentityPickerSearchControl>BaseControl.createIn(IdentityPickerSearchControl, $('.reviewer-picker'), idOptions);
-const titleControl = <Combo>BaseControl.createIn(Combo, $(".title-box"), <IComboOptions>{mode: "text"});
-const startDateControl = <Combo>BaseControl.createIn(Combo, $(".start-date-box"), <IComboOptions>{type: "date-time"});
-const endDateControl = <Combo>BaseControl.createIn(Combo, $(".end-date-box"), <IComboOptions>{type: "date-time"});
-const repoControl = <Combo>BaseControl.createIn(Combo, $('.repo-picker'), {});
+    identityType: { User: true, Group: true },
+};
+const creatorControl = <IdentityPickerSearchControl>BaseControl.createIn(IdentityPickerSearchControl, $(".creator-picker"), idOptions);
+const reviewerControl = <IdentityPickerSearchControl>BaseControl.createIn(IdentityPickerSearchControl, $(".reviewer-picker"), idOptions);
+const titleControl = <Combo>BaseControl.createIn(Combo, $(".title-box"), <IComboOptions>{ mode: "text" });
+const startDateControl = <Combo>BaseControl.createIn(Combo, $(".start-date-box"), <IComboOptions>{ type: "date-time" });
+const endDateControl = <Combo>BaseControl.createIn(Combo, $(".end-date-box"), <IComboOptions>{ type: "date-time" });
+const repoControl = <Combo>BaseControl.createIn(Combo, $(".repo-picker"), {});
 
 
 function getValue(control: IdentityPickerSearchControl): IEntity {
     const resolvedEntities = control.getIdentitySearchResult().resolvedEntities;
     if (resolvedEntities && resolvedEntities.length === 1) {
         return resolvedEntities[0];
-    }   
-}
-function setValue(control: IdentityPickerSearchControl, displayName: string): IPromise<IEntity | null> {
-    const service = getService(IdentityService);
-    const mapResults = service.getIdentities(displayName, 
-        <IOperationScope>{AAD: true, IMS: true, Source: true}, 
-        <IEntityType>{Group: true, User: true});
-    const promises = Object.keys(mapResults).map(key => mapResults[key]);
-    return Q.all(promises).then((models) => {
-        for (let model of models) {
-            if (model.identities.length > 0) {
-                const identity = model.identities[0];
-                control.clear();
-                control.setEntities([identity], []);
-                return identity;
-            }
-        }
-        return null;
-    }, (error) => {
-        return null;
-    });
+    }
 }
 
 let repositories: GitRepository[];
@@ -62,29 +37,29 @@ getGitClient().getRepositories(VSS.getWebContext().project.id).then(
         repositories = repos.sort((a, b) => a.name.localeCompare(b.name));
         repoControl.setSource(repositories.map((r) => r.name));
     }
-)
+);
 function getSelectedRepo(): string | null {
     const idx = repoControl.getSelectedIndex();
     return idx < 0 ? null : repositories[idx].id;
 }
 
 
-//Query Logic
+// query Logic
 function createFilter(): (pullRequest: GitPullRequest) => boolean {
     const title = titleControl.getValue<string>().toLowerCase();
     const start = startDateControl.getValue<Date>();
     const end = endDateControl.getValue<Date>();
 
-    return (pullRequest: GitPullRequest) => 
-    (!title || pullRequest.title.toLowerCase().indexOf(title) >= 0)
-    && (!start || pullRequest.creationDate.getTime() >= start.getTime())
-    && (!end || pullRequest.creationDate.getTime() <= end.getTime());
+    return (pullRequest: GitPullRequest) =>
+        (!title || pullRequest.title.toLowerCase().indexOf(title) >= 0)
+        && (!start || pullRequest.creationDate.getTime() >= start.getTime())
+        && (!end || pullRequest.creationDate.getTime() <= end.getTime());
 }
 
 let allPullRequests: GitPullRequest[] = [];
-function runQuery(append = false) {
+function runQuery(append: boolean = false) {
     const creator = getValue(creatorControl);
-    const reviewer = getValue(reviewerControl); 
+    const reviewer = getValue(reviewerControl);
     const criteria: GitPullRequestSearchCriteria = {
         creatorId: creator && creator.localId,
         reviewerId: reviewer && reviewer.localId,
@@ -96,7 +71,7 @@ function runQuery(append = false) {
 
     };
     const projectId = VSS.getWebContext().project.id;
-    getGitClient().getPullRequestsByProject(projectId, criteria, null, append ? allPullRequests.length: 0, 100).then((pullRequests) => {
+    getGitClient().getPullRequestsByProject(projectId, criteria, null, append ? allPullRequests.length : 0, 100).then((pullRequests) => {
         if (append) {
             allPullRequests = allPullRequests.concat(pullRequests);
         } else {
@@ -109,7 +84,7 @@ function runQuery(append = false) {
     });
 }
 
-//Event Logic
+// event Logic
 creatorControl._bind(IdentityPickerSearchControl.VALID_INPUT_EVENT, () => runQuery());
 creatorControl._bind(IdentityPickerSearchControl.RESOLVED_INPUT_REMOVED_EVENT, () => runQuery());
 reviewerControl._bind(IdentityPickerSearchControl.VALID_INPUT_EVENT, () => runQuery());
@@ -119,22 +94,22 @@ statusControl._bind("change", () => {
         return;
     }
     runQuery();
-})
+});
 titleControl._bind("change", () => {
     renderResults(allPullRequests, createFilter(), () => runQuery(true));
-})
+});
 startDateControl._bind("change", () => {
     renderResults(allPullRequests, createFilter(), () => runQuery(true));
-})
+});
 endDateControl._bind("change", () => {
     renderResults(allPullRequests, createFilter(), () => runQuery(true));
-})
+});
 repoControl._bind("change", () => {
     if (repoControl.getSelectedIndex() < 0 && repoControl.getText()) {
         return;
     }
     runQuery();
-})
+});
 
 runQuery();
 
