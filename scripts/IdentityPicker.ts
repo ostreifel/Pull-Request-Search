e6505b2609f0fb5523e7896154326a0726656778
Otto Streifel <ottost@microsoft.com>
2016-12-15 20:58:29 -0800

    v1.7.20 teams api based identity picker

---
+++ scripts/IdentityPicker.ts
@@ -0,0 +1,105 @@
+import { IdentityRef } from "VSS/WebApi/Contracts";
+import { Combo } from "VSS/Controls/Combos";
+import { getClient } from "TFS/Core/RestClient";
+import { WebApiTeam } from "TFS/Core/Contracts";
+import * as Q from "q";
+
+export interface Identity {
+    guid: string;
+    display: string;
+    image: string;
+}
+/**
+ * Control for picking identities.
+ * 
+ * The actual in product identity picker is not available for extensions
+ */
+export class IdentityPicker extends Combo {
+    private static readonly sortedIdentities: IdentityRef[] = [];
+    private static readonly cachedIdentities: { [guid: string]: void } = {};
+    private static readonly allPickers: IdentityPicker[] = [];
+
+    constructor() {
+        super();
+        IdentityPicker.allPickers.push(this);
+    }
+
+    private static upateAllSources() {
+        const source = this.sortedIdentities.map(i => i.displayName);
+        for (let picker of this.allPickers) {
+            picker.setSource(source);
+        }
+    }
+
+    private static insertIdentity(ident: IdentityRef) {
+        let arr = this.sortedIdentities;
+        if (arr.length === 0) {
+            return 0;
+        }
+        let lowerBound = 0;
+        let upperBound = arr.length - 1;
+        let curIn = 0;
+        while (true) {
+            curIn = Math.floor((upperBound + lowerBound) / 2);
+            if (arr[curIn].displayName === ident.displayName) {
+                return curIn;
+            } else if (arr[curIn].displayName < ident.displayName) {
+                lowerBound = curIn + 1;
+                if (lowerBound > upperBound) {
+                    return curIn + 1;
+                }
+            } else {
+                upperBound = curIn - 1;
+                if (lowerBound > upperBound) {
+                    return curIn;
+                }
+            }
+        }
+    }
+    public static cacheIdentity(ident: IdentityRef, tentative: boolean = false) {
+        const present = ident.id in this.cachedIdentities;
+        if (present && tentative) {
+            return;
+        }
+        this.cachedIdentities[ident.id] = void 0;
+        let idx = this.insertIdentity(ident);
+        this.sortedIdentities.splice(idx, present ? 1 : 0, ident);
+        this.upateAllSources();
+    }
+
+    public static cacheAllIdentitiesInTeam(project: { id: string, name: string }, team: WebApiTeam): IPromise<void> {
+        this.cacheIdentity(<IdentityRef>{ displayName: `[${project.name}]\\${team.name}`, id: team.id }, true);
+        return getClient().getTeamMembers(project.id, team.id).then(members => {
+            members.map(m => this.cacheIdentity(m));
+            return void 0;
+        });
+    }
+
+    public static cacheAllIdentitiesInProject(project: { id: string, name: string }): IPromise<void> {
+        return this.cacheAllIdentitiesInProjectImpl(project, 0);
+    }
+    public static cacheAllIdentitiesInProjectImpl(project: { id: string, name: string }, skip: number) {
+        return getClient().getTeams(project.id, 100, skip).then(teams => {
+            const promises = teams.map(t => this.cacheAllIdentitiesInTeam(project, t));
+            if (teams.length === 100) {
+                promises.push(this.cacheAllIdentitiesInProjectImpl(project, skip + 100));
+            }
+            return Q.all(promises).then(() => void 0);
+        });
+    }
+    public static cacheAllIdentitiesInAllProjects(): IPromise<void> {
+        return getClient().getProjects().then(projects =>
+            Q.all(projects.map(p => this.cacheAllIdentitiesInProject(p))).then(
+                () => void 0
+            )
+        );
+    }
+
+    public selectedIdentityId(): string | null {
+        if (this.getSelectedIndex() < 0) {
+            return null;
+        }
+        return IdentityPicker.sortedIdentities[this.getSelectedIndex()].id;
+    }
+
+}
--- scripts/app.ts
+++ scripts/app.ts
@@ -1,10 +1,12 @@
 import { Combo, IComboOptions } from "VSS/Controls/Combos";
-import { IdentityPickerSearchControl, IIdentityPickerSearchOptions } from "VSS/Identities/Picker/Controls";
 import { BaseControl } from "VSS/Controls";
 import { getClient as getGitClient } from "TFS/VersionControl/GitRestClient";
 import { GitPullRequestSearchCriteria, PullRequestStatus, GitPullRequest, GitRepository } from "TFS/VersionControl/Contracts";
+import { IdentityRef } from "VSS/WebApi/Contracts";
 import { renderResults } from "./PullRequestsView";
-import { IEntity } from "VSS/Identities/Picker/RestClient";
+import { IdentityPicker } from "./IdentityPicker";
+
+IdentityPicker.cacheAllIdentitiesInAllProjects();
 
 // create controls
 const statusOptions: IComboOptions = {
@@ -13,23 +15,12 @@ const statusOptions: IComboOptions = {
 };
 const statusControl = <Combo>BaseControl.createIn(Combo, $(".status-picker"), statusOptions);
 
-const idOptions: IIdentityPickerSearchOptions = {
-    identityType: { User: true, Group: true },
-};
-const creatorControl = <IdentityPickerSearchControl>BaseControl.createIn(IdentityPickerSearchControl, $(".creator-picker"), idOptions);
-const reviewerControl = <IdentityPickerSearchControl>BaseControl.createIn(IdentityPickerSearchControl, $(".reviewer-picker"), idOptions);
+const creatorControl = <IdentityPicker>BaseControl.createIn(IdentityPicker, $(".creator-picker"), {});
+const reviewerControl = <IdentityPicker>BaseControl.createIn(IdentityPicker, $(".reviewer-picker"), {});
 const titleControl = <Combo>BaseControl.createIn(Combo, $(".title-box"), <IComboOptions>{ mode: "text" });
 const startDateControl = <Combo>BaseControl.createIn(Combo, $(".start-date-box"), <IComboOptions>{ type: "date-time" });
 const endDateControl = <Combo>BaseControl.createIn(Combo, $(".end-date-box"), <IComboOptions>{ type: "date-time" });
-const repoControl = <Combo>BaseControl.createIn(Combo, $(".repo-picker"), {});
-
-
-function getValue(control: IdentityPickerSearchControl): IEntity {
-    const resolvedEntities = control.getIdentitySearchResult().resolvedEntities;
-    if (resolvedEntities && resolvedEntities.length === 1) {
-        return resolvedEntities[0];
-    }
-}
+const repoControl = <Combo>BaseControl.createIn(Combo, $(".repo-picker"), <IComboOptions>{});
 
 let repositories: GitRepository[];
 getGitClient().getRepositories(VSS.getWebContext().project.id).then(
@@ -41,6 +32,15 @@ getGitClient().getRepositories(VSS.getWebContext().project.id).then(
 function getSelectedRepo(): string | null {
     const idx = repoControl.getSelectedIndex();
     return idx < 0 ? null : repositories[idx].id;
+}
+
+
+function cacheIdentitiesFromPr(pr: GitPullRequest) {
+    const cache = (ident: IdentityRef | null) => ident && IdentityPicker.cacheIdentity(ident);
+    cache(pr.autoCompleteSetBy);
+    cache(pr.closedBy);
+    cache(pr.createdBy);
+    pr.reviewers.map(r => cache(r));
 }
 
 
@@ -58,11 +58,9 @@ function createFilter(): (pullRequest: GitPullRequest) => boolean {
 
 let allPullRequests: GitPullRequest[] = [];
 function runQuery(append: boolean = false) {
-    const creator = getValue(creatorControl);
-    const reviewer = getValue(reviewerControl);
     const criteria: GitPullRequestSearchCriteria = {
-        creatorId: creator && creator.localId,
-        reviewerId: reviewer && reviewer.localId,
+        creatorId: creatorControl.selectedIdentityId(),
+        reviewerId: reviewerControl.selectedIdentityId(),
         status: PullRequestStatus[statusControl.getInputText()],
         sourceRefName: null,
         targetRefName: null,
@@ -72,6 +70,7 @@ function runQuery(append: boolean = false) {
     };
     const projectId = VSS.getWebContext().project.id;
     getGitClient().getPullRequestsByProject(projectId, criteria, null, append ? allPullRequests.length : 0, 100).then((pullRequests) => {
+        pullRequests.map(pr => cacheIdentitiesFromPr(pr));
         if (append) {
             allPullRequests = allPullRequests.concat(pullRequests);
         } else {
@@ -85,10 +84,16 @@ function runQuery(append: boolean = false) {
 }
 
 // event Logic
-creatorControl._bind(IdentityPickerSearchControl.VALID_INPUT_EVENT, () => runQuery());
-creatorControl._bind(IdentityPickerSearchControl.RESOLVED_INPUT_REMOVED_EVENT, () => runQuery());
-reviewerControl._bind(IdentityPickerSearchControl.VALID_INPUT_EVENT, () => runQuery());
-reviewerControl._bind(IdentityPickerSearchControl.RESOLVED_INPUT_REMOVED_EVENT, () => runQuery());
+creatorControl._bind("change", () => {
+    if (creatorControl.getSelectedIndex() >= 0 || !creatorControl.getText()) {
+        runQuery();
+    }
+});
+reviewerControl._bind("change", () => {
+    if (reviewerControl.getSelectedIndex() >= 0 || !reviewerControl.getText()) {
+        runQuery();
+    }
+});
 statusControl._bind("change", () => {
     if (statusControl.getSelectedIndex() < 0) {
         return;
@@ -105,10 +110,9 @@ endDateControl._bind("change", () => {
     renderResults(allPullRequests, createFilter(), () => runQuery(true));
 });
 repoControl._bind("change", () => {
-    if (repoControl.getSelectedIndex() < 0 && repoControl.getText()) {
-        return;
+    if (repoControl.getSelectedIndex() >= 0 || !repoControl.getText()) {
+        runQuery();
     }
-    runQuery();
 });
 
 runQuery();
