892004ae2a77902e5386b100abd23d07283e5259
Otto Streifel <ottost@microsoft.com>
2017-02-14 09:26:30 -0800

    Removes unused lines

--- scripts/loadContents.tsx
+++ scripts/loadContents.tsx
@@ -36,21 +36,12 @@ function getDiffItems(sourceId: string, targetId: string, repository: GitReposit
         versionOptions: GitVersionOptions.None,
         versionType: GitVersionType.Commit
     } as GitTargetVersionDescriptor;
-    // get getGitClient().getCommitDiffs does not work because it uses the wrong params
-    // uses           https://ottost.visualstudio.com/Board%20Control/_apis/git/repositories/ebdb7ac1-28ca-4ce1-bb3e-f17f7e3deaf7/diffs/commits?diffCommonCommit=false&%24top=100&%24skip=0&baseVersionDescriptor%5BbaseVersion%5D=61ef60b7701bb62d5fb9aa4eb5014854ae2b0e87&baseVersionDescriptor%5BbaseVersionOptions%5D=0&baseVersionDescriptor%5BbaseVersionType%5D=2&targetVersionDescriptor%5BtargetVersion%5D=856bdc2c75fb30cefee7b02c6ccc9bc8e73e187a&targetVersionDescriptor%5BtargetVersionOptions%5D=0&targetVersionDescriptor%5BtargetVersionType%5D=2
-    // when should be https://ottost.visualstudio.com/Board%20Control/_apis/git/repositories/ebdb7ac1-28ca-4ce1-bb3e-f17f7e3deaf7/diffs/commits?diffCommonCommit=false&$top=100&$skip=0&baseVersion=61ef60b7701bb62d5fb9aa4eb5014854ae2b0e87&baseVersionType=2&targetVersion=856bdc2c75fb30cefee7b02c6ccc9bc8e73e187a&targetVersionType=2
-    // basically uses targetVersionDescriptor%5BtargetVersion%5D=856bdc2c75fb30cefee7b02c6ccc9bc8e73e187a
-    // instead of     targetVersion=856bdc2c75fb30cefee7b02c6ccc9bc8e73e187a
     const skip = prev ? prev.changes.length : 0;
-
-    const host = VSS.getWebContext().host.uri;
-    const path = `${repository.project.name}/_apis/git/repositories/${repository.id}/difs/commits`;
-    const params = `$top=100&$skip=${skip}&baseVersion=${sourceId}&baseType=2&targetVersion=${targetId}&targetType=2`;
     return getGitClient().getCommitDiffs(repository.id,
         repository.project.name,
         false,
         100,
-        prev ? prev.changes.length : 0,
+        skip,
         source,
         target
     ).then(diffs => {
